aed66f1cf9d90b4224878fc8b70799da10f6df4e
jcedeno-m1 <jcedeno@noobsters.net>
2021-10-16 15:58:15 -0400

    wip

---
+++ src/main/java/me/aleiv/core/paper/map/MapSystemManager.java
@@ -0,0 +1,12 @@
+package me.aleiv.core.paper.map;
+
+/**
+ * A class that manages the map system. In is intended to be used to handle the
+ * abstractions of translating a pixel point from the player's view vector in to
+ * a pixel int the AB plane of the map the player is pointing act.
+ * 
+ * @author jcedeno
+ */
+public class MapSystemManager {
+
+}
