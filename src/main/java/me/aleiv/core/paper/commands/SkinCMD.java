7b2364e9e41bef0451f05088b1bf5bac2eafd72c
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2021-11-17 19:01:29 -0500

    small tweaks

--- src/main/java/me/aleiv/core/paper/commands/SkinCMD.java
+++ src/main/java/me/aleiv/core/paper/commands/SkinCMD.java
@@ -110,7 +110,6 @@ public class SkinCMD extends BaseCommand {
             }
 
             if (id != null) {
-                // changeSkin(player, id.toString(), variant);
                 if (variant.equalsIgnoreCase("original")) {
                     var skin = SkinToolApi.getCurrentUserSkin(id, false);
                     skinSwapper(player, skin.getValue(), skin.getSignature());
@@ -141,69 +140,7 @@ public class SkinCMD extends BaseCommand {
         } else {
             sender.sendMessage("§cPlayer not found.");
         }
-
     }
-    /*
-     * 
-     * @Default
-     * 
-     * @CommandCompletion("@variants") public void changeSkin(Player sender, String
-     * uuid, String variant) { // Check if user has skins already, othewise create
-     * new one
-     * 
-     * var previousSkins = SkinToolApi.getPlayerSkins(UUID.fromString(uuid));
-     * 
-     * if (previousSkins != null) { var iterator =
-     * previousSkins.getAsJsonArray("skins").iterator();
-     * 
-     * JsonObject finalSkin = null;
-     * 
-     * while (iterator.hasNext()) { var skin = iterator.next().getAsJsonObject();
-     * var skinName = skin.get("name").getAsString(); if
-     * (skinName.equalsIgnoreCase(variant)) { finalSkin = skin; break; } }
-     * 
-     * if (finalSkin != null) { // Change the user's skin var value =
-     * finalSkin.get("value").getAsString(); var signature =
-     * finalSkin.get("signature"); // If signature is null, then the skin hasn't
-     * been signed onto mojang servers // yet! if (signature == null) {
-     * sender.sendMessage("§cThis skin hasn't been signed onto Mojang servers yet!"
-     * ); return; } // Apply the skin to the player
-     * Bukkit.getScheduler().runTask(Core.getInstance(), () -> skinSwapper(sender,
-     * value, signature.getAsString()));
-     * 
-     * } else { // Notify the user that the skin they requested doesn't exist
-     * sender.sendMessage( Core.getMiniMessage().parse("<red>The skin <white>" +
-     * variant + "<red> does not exist!")); }
-     * 
-     * } else { // Notify the user that they don't have any skins
-     * sender.sendMessage( Core.getMiniMessage().
-     * parse("<red>You don't have any skins! \n<red>Generating them for you..."));
-     * 
-     * // ASk to generate the skins
-     * SkinToolApi.createPlayerSkins(UUID.fromString(uuid)); // Recall this method
-     * changeSkin(sender, uuid, variant); }
-     * 
-     * }
-     * 
-     * @Subcommand("generate") public void generateSkins(Player sender) { var
-     * previousSkins = SkinToolApi.getPlayerSkins(sender.getUniqueId()); if
-     * (previousSkins != null) { sender.sendMessage(Core.getMiniMessage().
-     * parse("<red>You already have a skin!")); var iterator =
-     * previousSkins.getAsJsonArray("skins").iterator();
-     * 
-     * while (iterator.hasNext()) { var skin = iterator.next().getAsJsonObject();
-     * var skinName = skin.get("name").getAsString(); var signature =
-     * skin.get("signature"); if (signature == null) {
-     * sender.sendMessage(Core.getMiniMessage().parse("<red>Your skin variant " +
-     * "<white>" + skinName + "<red> hasn't been signed onto Mojang servers yet!"));
-     * return; } } return; } else { // Generate the skins var skinCollection =
-     * SkinToolApi.createPlayerSkins(sender.getUniqueId()); if (skinCollection !=
-     * null) { sender.sendMessage(Core.getMiniMessage().parse(
-     * "<green>Your skins have been generated!\n<yellow>Please note that they still need to be signed by mojang, and that process might take upto <white>10<yellow> minutes."
-     * )); } else { sender.sendMessage(Core.getMiniMessage().
-     * parse("<red>An error occured while generating your skins!")); } } }
-     * 
-     */
 
     /**
      * Function that swaps a player's skin for a different one.
