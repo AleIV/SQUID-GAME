ffd739ea04968b9fd1a9b5746ea29f7cefc03a21
DolphinGamerYT <dolphingamerytof@gmail.com>
2022-01-18 12:08:19 -0800

    Added tp-guards and tp-players

--- src/main/java/me/aleiv/core/paper/commands/UtilsCMD.java
+++ src/main/java/me/aleiv/core/paper/commands/UtilsCMD.java
@@ -1,5 +1,6 @@
 package me.aleiv.core.paper.commands;
 
+import co.aikar.commands.annotation.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
@@ -7,12 +8,6 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Flags;
-import co.aikar.commands.annotation.Name;
-import co.aikar.commands.annotation.Optional;
-import co.aikar.commands.annotation.Subcommand;
 import lombok.NonNull;
 import me.aleiv.core.paper.AnimationTools;
 import me.aleiv.core.paper.Core;
@@ -132,6 +127,24 @@ public class UtilsCMD extends BaseCommand {
             armorStand.remove();
         }
 
+    }
+
+    @Subcommand("tp-guards")
+    public void onTpGuards(Player player) {
+        Bukkit.getOnlinePlayers().forEach(p -> {
+            if (instance.getGame().isGuard(p)) {
+                p.teleport(player);
+            }
+        });
+    }
+
+    @Subcommand("tp-players")
+    public void onTpPlayers(Player player) {
+        Bukkit.getOnlinePlayers().forEach(p -> {
+            if (instance.getGame().isPlayer(p)) {
+                p.teleport(player);
+            }
+        });
     }
 
 }
